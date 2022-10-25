f6a7061833ee44e8e9608336e9836090794a026f
JeremyLoh <jeremyloh03@gmail.com>
2022-10-25 22:19:56 +0800

    Create LanguageSelectHomePage.java

---
+++ src/main/java/com/cookieClickerBot/pages/LanguageSelectHomePage.java
@@ -0,0 +1,25 @@
+package com.cookieClickerBot.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+public class LanguageSelectHomePage {
+    private static final String PAGE_URL = "https://orteil.dashnet.org/cookieclicker/";
+    private final WebDriver driver;
+
+    @FindBy(how = How.ID, using = "langSelect-EN")
+    private WebElement englishLanguageSelectElement;
+
+    public LanguageSelectHomePage(WebDriver driver) {
+        this.driver = driver;
+        this.driver.get(PAGE_URL);
+        PageFactory.initElements(this.driver, this);
+    }
+
+    public void setGameLanguage() {
+        englishLanguageSelectElement.click();
+    }
+}
