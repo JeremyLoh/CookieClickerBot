8fd866037e546d3cacb3c9e170eb49543ec313db
JeremyLoh <jeremyloh03@gmail.com>
2022-10-27 00:15:43 +0800

    Update test lifecycle

--- src/main/java/com/cookieClickerBot/tests/CookieClickerGameTest.java
+++ src/main/java/com/cookieClickerBot/tests/CookieClickerGameTest.java
@@ -3,8 +3,9 @@ package com.cookieClickerBot.tests;
 import com.cookieClickerBot.pages.HomePage;
 import com.cookieClickerBot.pages.LanguageSelectHomePage;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -16,14 +17,18 @@ public class CookieClickerGameTest {
     private static final String PAGE_URL = "https://orteil.dashnet.org/cookieclicker/";
 
     @BeforeAll
-    static void setup() {
+    static void setupBeforeAll() {
         WebDriverManager.firefoxdriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
         driver = new FirefoxDriver();
     }
 
-    @AfterAll
-    static void teardown() throws InterruptedException {
-        Thread.sleep(5000);
+    @AfterEach
+    void teardown() throws InterruptedException {
+        Thread.sleep(1000);
         driver.close();
     }
 
