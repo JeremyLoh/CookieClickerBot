a3bc311a9eeee90141a3e5acebbcbc4dfe52002b
JeremyLoh <jeremyloh03@gmail.com>
2022-10-29 23:37:42 +0800

    Add test for buying first upgrade

--- src/main/java/com/cookieClickerBot/tests/CookieClickerGameTest.java
+++ src/main/java/com/cookieClickerBot/tests/CookieClickerGameTest.java
@@ -2,6 +2,7 @@ package com.cookieClickerBot.tests;
 
 import com.cookieClickerBot.pages.HomePage;
 import com.cookieClickerBot.pages.LanguageSelectHomePage;
+import com.cookieClickerBot.pages.UpgradePage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,9 +10,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CookieClickerGameTest {
     private static WebDriver driver;
@@ -45,5 +48,20 @@ public class CookieClickerGameTest {
         assertEquals(0, homePage.getCurrentCookieCount());
         homePage.clickCookie();
         assertEquals(1, homePage.getCurrentCookieCount());
+    }
+
+    @Test
+    void buyFirstUpgrade() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(500));
+        HomePage homePage = new HomePage(driver);
+        UpgradePage upgradePage = new UpgradePage(driver);
+        assertEquals(0, homePage.getCurrentCookieCount());
+        for (int i = 0; i < 15; i++) {
+            homePage.clickCookie();
+        }
+        assertEquals(0, homePage.getCookiesPerSecond());
+        wait.until((driver) -> homePage.getCurrentCookieCount() == 15);
+        upgradePage.clickMostExpensiveUpgrade(15);
+        wait.until((driver) -> homePage.getCookiesPerSecond() > 0);
     }
 }
